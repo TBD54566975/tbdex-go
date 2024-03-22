72daced0701ea6d216edbb5561b2d15a4890c365
Phoebe Lew <plew@squareup.com>
2024-03-21 22:07:09 -0700

    precompile

--- protocol/validator.go
+++ protocol/validator.go
@@ -1,40 +1,51 @@
 package protocol
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/santhosh-tekuri/jsonschema/v5"
 )
 
-var schemaPaths = map[string]string{
-    "definitions": "../tbdex/hosted/json-schemas/definitions.json",
-    "resource":    "../tbdex/hosted/json-schemas/resource.schema.json",
-    "offering":    "../tbdex/hosted/json-schemas/offering.schema.json",
-    "balance":     "../tbdex/hosted/json-schemas/balance.schema.json",
-    "message":     "../tbdex/hosted/json-schemas/message.schema.json",
-    "order":       "../tbdex/hosted/json-schemas/order.schema.json",
-    "orderstatus": "../tbdex/hosted/json-schemas/orderstatus.schema.json",
-    "Quote":       "../tbdex/hosted/json-schemas/quote.schema.json",
-    "rfq":         "../tbdex/hosted/json-schemas/rfq.schema.json",
-    "close":       "../tbdex/hosted/json-schemas/close.schema.json",
+var ValidatorMap map[string]*jsonschema.Schema = make(map[string]*jsonschema.Schema)
+
+type schemaObject struct {
+	URL  string
+	Path string
 }
 
-func Compiler() (*jsonschema.Compiler, error) {
+var schemas = map[string]schemaObject{
+	"definitions": {"https://tbdex.dev/definitions.json", "../tbdex/hosted/json-schemas/definitions.json"},
+	"resource":    {"https://tbdex.dev/resource.schema.json", "../tbdex/hosted/json-schemas/resource.schema.json"},
+	"offering":    {"https://tbdex.dev/offering.schema.json", "../tbdex/hosted/json-schemas/offering.schema.json"},
+	"balance":     {"https://tbdex.dev/balance.schema.json", "../tbdex/hosted/json-schemas/balance.schema.json"},
+	"message":     {"https://tbdex.dev/message.schema.json", "../tbdex/hosted/json-schemas/message.schema.json"},
+	"order":       {"https://tbdex.dev/order.schema.json", "../tbdex/hosted/json-schemas/order.schema.json"},
+	"orderstatus": {"https://tbdex.dev/orderstatus.schema.json", "../tbdex/hosted/json-schemas/orderstatus.schema.json"},
+	"quote":       {"https://tbdex.dev/Quote.schema.json", "../tbdex/hosted/json-schemas/Quote.schema.json"},
+	"rfq":         {"https://tbdex.dev/rfq.schema.json", "../tbdex/hosted/json-schemas/rfq.schema.json"},
+	"close":       {"https://tbdex.dev/close.schema.json", "../tbdex/hosted/json-schemas/close.schema.json"},
+}
+
+func init() {
     compiler := jsonschema.NewCompiler()
-    
-    for _, path := range schemaPaths {
-        file, err := os.Open(path)
+
+	for name, schemaObject := range schemas {
+		file, err := os.Open(schemaObject.Path)
 		if err != nil {
-            return nil, err
-        }
+            fmt.Println("Failed to open schema file", err)
+		}
 
-        if err := compiler.AddResource(path, file); err != nil {
-            return nil, err
-        }
+		if err := compiler.AddResource(schemaObject.URL, file); err != nil {
+            fmt.Println("Failed to add schema file", err)
+		}
 
-        file.Close()
+		file.Close()
+
+        schema, err := compiler.Compile(schemas["resource"].URL)
+        if err != nil {
+            fmt.Println("Failed to compile schema", err)
+        }
+        ValidatorMap[name] = schema
 	}
-	
-    return compiler, nil
 }
-
--- protocol/validator_test.go
+++ protocol/validator_test.go
@@ -2,81 +2,19 @@ package protocol
 
 import (
 	"encoding/json"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/TBD54566975/tbdex-go/protocol/resource/offering"
 	"github.com/alecthomas/assert"
-	"github.com/santhosh-tekuri/jsonschema/v5"
-	_ "github.com/santhosh-tekuri/jsonschema/v5/httploader"
 	"github.com/tbd54566975/web5-go/dids/didjwk"
 )
 
-func TestValidator(t *testing.T) {
-    definitionsPath := "../tbdex/hosted/json-schemas/definitions.json"
-	resourcePath := "../tbdex/hosted/json-schemas/resource.schema.json"
-
-    definitionsFile, err := os.Open(definitionsPath)
-    resourceFile, err := os.Open(resourcePath)
-    if err != nil {
-		panic(err)
-	}
-
-
-	// 1. Load the JSON schema
-	compiler := jsonschema.NewCompiler()
-	err = compiler.AddResource(definitionsPath, definitionsFile)
-
-	assert.NoError(t, err)
-
-    if err := compiler.AddResource(resourcePath, resourceFile); err != nil {
-		panic(err)
-	}
-    schema, err := compiler.Compile(resourcePath)
-	assert.NoError(t, err)
-
-    bearerDID, err := didjwk.Create()
-	assert.NoError(t, err)
-
-	offering, err := offering.Create(
-		offering.WithPayin(
-			"USD",
-			offering.WithPayinMethod("SQUAREPAY"),
-		),
-		offering.WithPayout(
-			"USDC",
-			offering.WithPayoutMethod(
-				"STORED_BALANCE",
-				20*time.Minute,
-			),
-		),
-		"1.0",
-        bearerDID.URI,
-	)
-
-    offeringJSON, err := json.Marshal(offering)
-	if err != nil {
-		panic(err)
-	}
-
-    // 4. Validate the JSON against the schema
-	var v interface{}
-	err = json.Unmarshal([]byte(offeringJSON), &v)
-    assert.NoError(t, err)
-
-	err = schema.Validate(v)
-    assert.NoError(t, err)
-}
-
 func TestCompiler(t *testing.T) {
-    compiler, err := Compiler()
-    assert.NoError(t, err)
-
-    bearerDID, err := didjwk.Create()
+	bearerDID, err := didjwk.Create()
 	assert.NoError(t, err)
 
-	offering, err := offering.Create(
+	offering, _ := offering.Create(
 		offering.WithPayin(
 			"USD",
 			offering.WithPayinMethod("SQUAREPAY"),
@@ -89,22 +27,17 @@ func TestCompiler(t *testing.T) {
 			),
 		),
 		"1.0",
-        bearerDID.URI,
+		bearerDID.URI,
 	)
 
-    offeringJSON, err := json.Marshal(offering)
-	if err != nil {
-		panic(err)
-	}
+	offeringJSON, err := json.Marshal(offering)
+	assert.NoError(t, err)
 
-    // 4. Validate the JSON against the schema
-    schema, err := compiler.Compile(schemaPaths["resource"])
-    assert.NoError(t, err)
+	var v interface{}
+	err = json.Unmarshal(offeringJSON, &v)
+	assert.NoError(t, err)
 
-    var v interface{}
-	err = json.Unmarshal([]byte(offeringJSON), &v)
-    assert.NoError(t, err)
-
+    schema := ValidatorMap["resource"]
 	err = schema.Validate(v)
-    assert.NoError(t, err)
+	assert.NoError(t, err)
 }
