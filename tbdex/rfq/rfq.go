15e32afd7b6ceadb961138aed9a39cef5a42d32e
Phoebe Lew <plew@squareup.com>
2024-04-12 09:08:32 +1000

    stash

---
+++ tbdex/rfq/rfq.go
@@ -0,0 +1,144 @@
+package rfq
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/TBD54566975/tbdex-go/tbdex"
+	"github.com/tbd54566975/web5-go/dids/did"
+)
+
+// RFQKind identifies this message kind
+const RFQKind = "rfq"
+
+// RFQ represents a request for quote message within the exchange.
+type RFQ struct {
+	MessageMetadata tbdex.MessageMetadata `json:"metadata"`
+	Data            RFQData               `json:"data"`
+	PrivateData     *RFQPrivateData       `json:"privateData,omitempty"`
+	Signature       string                `json:"signature"`
+}
+
+// RFQData encapsulates the data content of a request for quote.
+type RFQData struct {
+	OfferingID string               `json:"offeringId"`
+	Payin      SelectedPayinMethod  `json:"payin"`
+	Payout     SelectedPayoutMethod `json:"payout"`
+	ClaimsHash string               `json:"claimsHash,omitempty"`
+}
+
+// RFQPrivateData contains data which can be detached from the payload without disrupting integrity.
+type RFQPrivateData struct {
+	Salt   string                 `json:"salt"`
+	Claims []string               `json:"claims"`
+	Payin  *PrivatePaymentDetails `json:"payin"`
+	Payout *PrivatePaymentDetails `json:"payout"`
+}
+
+// PrivatePaymentDetails is a container for the cleartest [PaymentDetails]
+type PrivatePaymentDetails struct {
+	PaymentDetails map[string]any `json:"paymentDetails"`
+}
+
+// SelectedPayinMethod represents the chosen method for the pay-in
+type SelectedPayinMethod struct {
+	Amount             string `json:"amount"`
+	Kind               string `json:"kind"`
+	PaymentDetailsHash string `json:"paymentDetailsHash,omitempty"`
+}
+
+// SelectedPayoutMethod represents the chosen method for the pay-out
+type SelectedPayoutMethod struct {
+	Kind               string `json:"kind"`
+	PaymentDetailsHash string `json:"paymentDetailsHash,omitempty"`
+}
+
+// Digest computes a hash of the rfq
+func (r RFQ) Digest() ([]byte, error) {
+	payload := map[string]any{"metadata": r.MessageMetadata, "data": r.Data}
+
+	hashed, err := tbdex.DigestJSON(payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to digest rfq: %w", err)
+	}
+
+	return hashed, nil
+}
+
+// Sign cryptographically signs the RFQ using DID's private key
+func (r *RFQ) Sign(bearerDID did.BearerDID) error {
+	r.MessageMetadata.From = bearerDID.URI
+
+	signature, err := tbdex.Sign(r, bearerDID)
+	if err != nil {
+		return fmt.Errorf("failed to sign rfq: %w", err)
+	}
+
+	r.Signature = signature
+
+	return nil
+}
+
+// UnmarshalJSON validates and unmarshals the input data into an RFQ.
+func (r *RFQ) UnmarshalJSON(data []byte, requireAllPrivateData bool) error {
+	err := tbdex.Validate(tbdex.TypeMessage, data, tbdex.WithKind(RFQKind))
+	if err != nil {
+		return fmt.Errorf("invalid rfq: %w", err)
+	}
+
+	ret := rfq{}
+	err = json.Unmarshal(data, &ret)
+	if err != nil {
+		return fmt.Errorf("failed to JSON unmarshal rfq: %w", err)
+	}
+
+	*r = RFQ(ret)
+
+	_, err = tbdex.VerifySignature(r, r.Signature)
+	if err != nil {
+		return fmt.Errorf("failed to verify RFQ signature: %w", err)
+	}
+
+	// TODO add check when decoded.SignerDID is implemented
+	// if decoded.SignerDID != r.MessageMetadata.From {
+	// 	return errors.New("signer: %w does not match message metadata from: %w", decoded.Header.SignerDID, r.MessageMetadata.From)
+	// }
+
+	if requireAllPrivateData {
+		err = r.verifyAllPrivateData()
+		if err != nil {
+			return fmt.Errorf("failed to verify private data: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (r *RFQ) verifyAllPrivateData() error {
+	if r.PrivateData == nil {
+		return errors.New("private data is missing")
+	}
+
+	if r.Data.ClaimsHash == "" {
+		return errors.New("required claims hash is missing")
+	}
+	payload := []any{r.PrivateData.Salt, r.PrivateData.Claims}
+	if err := tbdex.VerifyDigest(r.Data.ClaimsHash, payload); err != nil { return err }
+
+	if r.Data.Payin.PaymentDetailsHash == "" {
+		return errors.New("required payin details hash is missing")
+	}
+	payload = []any{r.PrivateData.Salt, r.PrivateData.Payin}
+	if err := tbdex.VerifyDigest(r.Data.Payin.PaymentDetailsHash, payload); err != nil { return err }
+
+	if r.Data.Payout.PaymentDetailsHash == "" {
+		return errors.New("required payout details hash is missing")
+	}
+	payload = []any{r.PrivateData.Salt, r.PrivateData.Payout}
+	if err := tbdex.VerifyDigest(r.Data.Payout.PaymentDetailsHash, payload); err != nil { return err }
+
+	return nil
+}
+
+type rfq RFQ
---
+++ tbdex/rfq/rfq_test.go
@@ -0,0 +1,112 @@
+package rfq_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/TBD54566975/tbdex-go/tbdex/offering"
+	"github.com/TBD54566975/tbdex-go/tbdex/rfq"
+	"github.com/alecthomas/assert/v2"
+	"github.com/tbd54566975/web5-go/dids/didjwk"
+	"go.jetpack.io/typeid"
+)
+
+func TestCreateRFQ(t *testing.T) {
+	pfiDID, err := didjwk.Create()
+	assert.NoError(t, err)
+
+	walletDID, err := didjwk.Create()
+	assert.NoError(t, err)
+
+	offeringID, err := typeid.WithPrefix(offering.Kind)
+	assert.NoError(t, err)
+
+	_, err = rfq.CreateRFQ(
+		walletDID.URI,
+		pfiDID.URI,
+		offeringID.String(),
+		rfq.WithRFQSelectedPayinMethod("100", "STORED_BALANCE"),
+		rfq.WithRFQSelectedPayoutMethod("BANK_ACCOUNT"),
+		rfq.WithRFQExternalID("test_1234"),
+	)
+
+	assert.NoError(t, err)
+}
+
+func TestCreateRFQ_WithPrivate(t *testing.T) {
+	pfiDID, _ := didjwk.Create()
+	walletDID, _ := didjwk.Create()
+	offeringID, _ := typeid.WithPrefix(offering.Kind)
+
+	rfq, err := rfq.CreateRFQ(
+		walletDID.URI,
+		pfiDID.URI,
+		offeringID.String(),
+		rfq.WithRFQSelectedPayinMethod("100", "STORED_BALANCE"),
+		rfq.WithRFQSelectedPayoutMethod("BANK_ACCOUNT", rfq.WithPayoutMethodWithDetails(
+			map[string]interface{}{
+				"accountNumber": "1234567890123456",
+				"routingNumber": "123456789",
+			})),
+		rfq.WithRFQClaims([]string{"my_jwt"}),
+	)
+
+	assert.NoError(t, err)
+	assert.NotZero(t, rfq.Data.Payout.PaymentDetailsHash)
+	assert.NotZero(t, rfq.Data.ClaimsHash)
+}
+
+func TestRFQ_Sign(t *testing.T) {
+	pfiDID, _ := didjwk.Create()
+
+	walletDID, _ := didjwk.Create()
+	offeringID, _ := typeid.WithPrefix(offering.Kind)
+
+	r, _ := rfq.CreateRFQ(
+		walletDID.URI,
+		pfiDID.URI,
+		offeringID.String(),
+		rfq.WithRFQSelectedPayinMethod("100", "STORED_BALANCE"),
+		rfq.WithRFQSelectedPayoutMethod("BANK_ACCOUNT"),
+	)
+
+	err := r.Sign(walletDID)
+	assert.NoError(t, err)
+}
+
+func TestRFQ_UnmarshalJSON(t *testing.T) {
+	pfiDID, _ := didjwk.Create()
+
+	walletDID, _ := didjwk.Create()
+	offeringID, _ := typeid.WithPrefix(offering.Kind)
+
+	r, _ := rfq.CreateRFQ(
+		walletDID.URI,
+		pfiDID.URI,
+		offeringID.String(),
+		rfq.WithRFQSelectedPayinMethod("100", "STORED_BALANCE"),
+		rfq.WithRFQSelectedPayoutMethod("BANK_ACCOUNT", rfq.WithPayoutMethodWithDetails(
+			map[string]interface{}{
+				"accountNumber": "1234567890123456",
+				"routingNumber": "123456789",
+			})),
+		rfq.WithRFQClaims([]string{"my_jwt"}),
+	)
+
+	_ = r.Sign(walletDID)
+
+	bytes, err := json.Marshal(r)
+	assert.NoError(t, err)
+
+	var rfq rfq.RFQ
+	err = rfq.UnmarshalJSON(bytes, true)
+	assert.NoError(t, err)
+}
+
+func TestRFQ_Unmarshal_Invalid(t *testing.T) {
+	input := []byte(`{"doo": "doo"}`)
+
+	var rfq rfq.RFQ
+	err := rfq.UnmarshalJSON(input, true)
+	assert.Error(t, err)
+}
