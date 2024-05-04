8622cccbcbf1cac735c111df1dea32e5483bdd58
Moe Jangda <moe@tbd.email>
2024-05-04 15:04:16 -0500

    uncomment test
    Co-Authored-By: phoebe-lew <lew.huiying@gmail.com>
    Co-Authored-By: Diane Huxley <dhuxley@tbd.email>

--- tbdex/validator/validator_test.go
+++ tbdex/validator/validator_test.go
@@ -1,6 +1,13 @@
 package validator_test
 
-// func TestValidate_Invalid(t *testing.T) {
-// 	err := tbdex.Validate(tbdex.TypeResource, []byte(`{"foo": "bar"}`))
-// 	assert.Error(t, err)
-// }
+import (
+	"testing"
+
+	"github.com/TBD54566975/tbdex-go/tbdex/validator"
+	"github.com/alecthomas/assert"
+)
+
+func TestValidate_Invalid(t *testing.T) {
+	err := validator.Validate(validator.TypeResource, []byte(`{"foo": "bar"}`))
+	assert.Error(t, err)
+}
