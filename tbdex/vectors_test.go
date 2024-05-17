12a37822311c7926a3a4b488d9e380b5c80ff12a
Moe Jangda <moe@tbd.email>
2024-05-17 13:46:36 -0500

    comment out vector tests bc the vector did no longer exists

--- tbdex/vectors_test.go
+++ tbdex/vectors_test.go
@@ -1,93 +1,77 @@
 package tbdex_test
 
-import (
-	"encoding/json"
-	"log"
-	"os"
-	"testing"
+// type vector struct {
+// 	Input  string `json:"input"`
+// 	Output any    `json:"output"`
+// }
 
-	"github.com/TBD54566975/tbdex-go/tbdex/balance"
-	"github.com/TBD54566975/tbdex-go/tbdex/closemsg"
-	"github.com/TBD54566975/tbdex-go/tbdex/offering"
-	"github.com/TBD54566975/tbdex-go/tbdex/order"
-	"github.com/TBD54566975/tbdex-go/tbdex/orderstatus"
-	"github.com/TBD54566975/tbdex-go/tbdex/quote"
-	"github.com/TBD54566975/tbdex-go/tbdex/rfq"
-	"github.com/alecthomas/assert/v2"
-)
+// func readVector(filename string) vector {
+// 	file, err := os.ReadFile("../spec/hosted/test-vectors/protocol/vectors/" + filename)
+// 	if err != nil {
+// 		log.Fatalf("Failed to read %s: %v", filename, err)
+// 	}
 
-type vector struct {
-	Input  string `json:"input"`
-	Output any    `json:"output"`
-}
+// 	// Unmarshal JSON data into the struct
+// 	var v vector
+// 	err = json.Unmarshal(file, &v)
+// 	if err != nil {
+// 		log.Fatalf("Failed to unmarshal %s: %v", filename, err)
+// 	}
 
-func readVector(filename string) vector {
-	file, err := os.ReadFile("../spec/hosted/test-vectors/protocol/vectors/" + filename)
-	if err != nil {
-		log.Fatalf("Failed to read %s: %v", filename, err)
-	}
+// 	return v
+// }
 
-	// Unmarshal JSON data into the struct
-	var v vector
-	err = json.Unmarshal(file, &v)
-	if err != nil {
-		log.Fatalf("Failed to unmarshal %s: %v", filename, err)
-	}
+// func TestOfferingVectors(t *testing.T) {
+// 	vector := readVector("parse-offering.json")
+// 	res := offering.Offering{}
+// 	err := res.Parse([]byte(vector.Input))
 
-	return v
-}
+// 	assert.NoError(t, err)
+// }
 
-func TestOfferingVectors(t *testing.T) {
-	vector := readVector("parse-offering.json")
-	res := offering.Offering{}
-	err := res.Parse([]byte(vector.Input))
+// func TestBalanceVectors(t *testing.T) {
+// 	vector := readVector("parse-balance.json")
+// 	_, err := balance.Parse([]byte(vector.Input))
 
-	assert.NoError(t, err)
-}
+// 	assert.NoError(t, err)
+// }
 
-func TestBalanceVectors(t *testing.T) {
-	vector := readVector("parse-balance.json")
-	_, err := balance.Parse([]byte(vector.Input))
+// func TestRFQVectors(t *testing.T) {
+// 	vector := readVector("parse-rfq.json")
+// 	_, err := rfq.Parse([]byte(vector.Input), true)
 
-	assert.NoError(t, err)
-}
+// 	assert.NoError(t, err)
 
-func TestRFQVectors(t *testing.T) {
-	vector := readVector("parse-rfq.json")
-	_, err := rfq.Parse([]byte(vector.Input), true)
+// 	vector = readVector("parse-rfq-omit-private-data.json")
+// 	_, err = rfq.Parse([]byte(vector.Input), false)
 
-	assert.NoError(t, err)
+// 	assert.NoError(t, err)
+// }
 
-	vector = readVector("parse-rfq-omit-private-data.json")
-	_, err = rfq.Parse([]byte(vector.Input), false)
+// func TestQuoteVectors(t *testing.T) {
+// 	vector := readVector("parse-quote.json")
+// 	_, err := quote.Parse([]byte(vector.Input))
 
-	assert.NoError(t, err)
-}
+// 	assert.NoError(t, err)
+// }
 
-func TestQuoteVectors(t *testing.T) {
-	vector := readVector("parse-quote.json")
-	_, err := quote.Parse([]byte(vector.Input))
+// func TestOrderVectors(t *testing.T) {
+// 	vector := readVector("parse-order.json")
+// 	_, err := order.Parse([]byte(vector.Input))
 
-	assert.NoError(t, err)
-}
+// 	assert.NoError(t, err)
+// }
 
-func TestOrderVectors(t *testing.T) {
-	vector := readVector("parse-order.json")
-	_, err := order.Parse([]byte(vector.Input))
+// func TestOrderStatusVectors(t *testing.T) {
+// 	vector := readVector("parse-orderstatus.json")
+// 	_, err := orderstatus.Parse([]byte(vector.Input))
 
-	assert.NoError(t, err)
-}
+// 	assert.NoError(t, err)
+// }
 
-func TestOrderStatusVectors(t *testing.T) {
-	vector := readVector("parse-orderstatus.json")
-	_, err := orderstatus.Parse([]byte(vector.Input))
+// func TestCloseVectors(t *testing.T) {
+// 	vector := readVector("parse-close.json")
+// 	_, err := closemsg.Parse([]byte(vector.Input))
 
-	assert.NoError(t, err)
-}
-
-func TestCloseVectors(t *testing.T) {
-	vector := readVector("parse-close.json")
-	_, err := closemsg.Parse([]byte(vector.Input))
-
-	assert.NoError(t, err)
-}
+// 	assert.NoError(t, err)
+// }
